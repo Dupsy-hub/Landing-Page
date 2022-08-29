877b8b1a36ef74eecd9a5a93a1da6e133c607d2d
Hans Tognon <learningway403@gmail.com>
2022-08-29 14:17:03 +0100

    update to use Intersection Observer API

--- js/app.js
+++ js/app.js
@@ -4,7 +4,6 @@ const sections = document.querySelectorAll("section");
 
 // build the nav
 const buildNav = () => {
-  //for loop to create sections as number of sections in HTML file
   for (let i = 0; i < sections.length; i++) {
     const sectionId = sections[i].getAttribute("id");
     const sectionName = sections[i].getAttribute("data-nav");
@@ -12,63 +11,36 @@ const buildNav = () => {
   }
 };
 
-// Getting the largest value that's less or equal to the number
-const offset = (section) => {
-  return Math.floor(section.getBoundingClientRect().top);
+// Using IntersectionObserver API
+// Discover it at https://developer.mozilla.org/en-US/docs/Web/API/Intersection_Observer_API
+const observerOptions = {
+  rootMargin: "0px",
+  threshold: [0.55],
 };
 
-// add the active class
-const addActiveSection = (conditional, section) => {
-  if (conditional) {
-    section.classList.add("active");
-    section.style.backgroundColor = "#FF0000";
-    // extract index from section id
-    const sectionId = +section.id[section.id.length - 1] - 1;
-    //add background-color to active navigation
-    navMenu.childNodes[sectionId].style.backgroundColor = "#FF0000";
+// Callback executed when a section becomes visible
+const observerCallback = (entries) => {
+  for (let i = 0; i < entries.length; i++) {
+    const target = entries[i].target;
+    const sectionId = +target.id[target.id.length - 1] - 1;
+
+    if (entries[i].isIntersecting) {
+      target.classList.add("active");
+      target.style.backgroundColor = "#FF0000";
+      navMenu.childNodes[sectionId].style.backgroundColor = "#FF0000";
+    } else {
+      target.style.backgroundColor = "";
+      navMenu.childNodes[sectionId].style.backgroundColor = "";
+    }
   }
 };
 
-// remove the active class
-const removeActiveSection = (section) => {
-  section.classList.remove("active");
-  section.style.backgroundColor = "";
-  // extract index from section id
-  const sectionId = +section.id[section.id.length - 1] - 1;
-  //remove background-color to active navigation
-  navMenu.childNodes[sectionId].style.backgroundColor = "white";
-};
-
-// implementing the sectionActivation function
-const sectionActivation = () => {
-  sections.forEach((section) => {
-    const elementOffset = offset(section);
-
-    removeActiveSection(section);
-    addActiveSection(elementOffset < 150 && elementOffset >= -150, section);
-  });
-};
-
-// scroll to anchor ID using scrollTO event
-const scrolling = () => {
-  const links = document.querySelectorAll(".navbar__menu a");
-  links.forEach((link) => {
-    link.addEventListener("click", (event) => {
-      for (i = 0; i < sections; i++) {
-        sections[i].addEventListener("click", sectionScroll(link));
-        event.preventDefault();
-        document.querySelector(this.getAttribute("link")).scrollIntoView({
-          behavior: "smooth",
-        });
-      }
-    });
-  });
-};
-
-// configure listener once th epage has been loaded
+// configure listener once the page has been loaded
 document.addEventListener("DOMContentLoaded", () => {
-  buildNav();
-  scrolling();
+  const observer = new IntersectionObserver(observerCallback, observerOptions);
 
-  window.addEventListener("scroll", sectionActivation);
+  buildNav();
+
+  // Register each section to the observer
+  sections.forEach((section) => observer.observe(section));
 });
