1d071a3d6e0fbf69fb93d69a22432b92f3fd48ac
Hans Tognon <learningway403@gmail.com>
2022-08-29 11:01:56 +0100

    refactored

--- README.md
+++ README.md
@@ -1,22 +1,26 @@
 # Project Title
+
 Landing Page Project
 
 ## Project Description
-Building a multi-sectional landing page and converting a static web project to a dynamic project using JavaScript. 
+
+Building a multi-sectional landing page and converting a static web project to a dynamic project using JavaScript.
 
 ## Languages used
+
 - CSS
 - HTML
 - JAVASCRIPT
 
 ## Functionalities
+
 - The Navigation
 - The Dynamic Active Class
 - The Scrolling Effect
 
 ## Instructions on how to use
+
 - Open your browser
 - Click on 'Ctrl + O' to open
 - Navigate to the index.html
 - Then click on it
-
--- js/app.js
+++ js/app.js
@@ -11,21 +11,20 @@
  *
  * JS Standard: ESlint
  *
-*/
+ */
 
 /**
  * Comments should be present at the beginning of each procedure and class.
  * Great to have comments before crucial code sections within the procedure.
-*/
+ */
 
 /**
  * Define Global Variables
  *
-*/
+ */
 // Nav Menu Global Variables
 const navMenu = document.getElementById("navbar__list");
-const sections =document.querySelectorAll("section");
-const fragment = document.createDocumentFragment();
+const sections = document.querySelectorAll("section");
 
 /**
  * End Global Variables
@@ -35,77 +34,74 @@ const fragment = document.createDocumentFragment();
 */
 
 // build the nav
-const buildNav= () => {
-    //for loop to create sections as number of sections in HTML file
-    for (let i=0; i < sections.length; i++) {
-        const sectionId = sections[i].getAttribute('id');
-        const sectionName = sections[i].getAttribute('data-nav');
-        navMenu.innerHTML += `<li><a class= "menu__link" href= "#${sectionId}">${sectionName}</a></li>`
-    }
+const buildNav = () => {
+  //for loop to create sections as number of sections in HTML file
+  for (let i = 0; i < sections.length; i++) {
+    const sectionId = sections[i].getAttribute("id");
+    const sectionName = sections[i].getAttribute("data-nav");
+    navMenu.innerHTML += `<li><a class= "menu__link" href= "#${sectionId}">${sectionName}</a></li>`;
+  }
 };
-
 
 // Getting the largest value that's less or equal to the number
 const offset = (section) => {
-    return Math.floor(section.getBoundingClientRect().top);
- };
-
- // add the active class
-const addActive = (conditional, section) => {
-    if(conditional) {
-        section.classList.add('active');
-        section.style.backgroundColor = "#FF0000";
-        // extract index from section id
-        const sectionId = +section.id[section.id.length - 1] - 1;
-        //add background-color to active navigation
-        navMenu.childNodes[sectionId].style.backgroundColor="#FF0000";
-    };
+  return Math.floor(section.getBoundingClientRect().top);
 };
 
- // remove the active class
-const removeActive = (section) => {
-    section.classList.remove('active');
-    section.style.backgroundColor = "linear-gradient(0deg, rgba(255,255,255,.1) 0%, rgba(255,255,255,.2) 100%)";
+// add the active class
+const addActiveSection = (conditional, section) => {
+  if (conditional) {
+    section.classList.add("active");
+    section.style.backgroundColor = "#FF0000";
     // extract index from section id
     const sectionId = +section.id[section.id.length - 1] - 1;
-    //remove background-color to active navigation
-    navMenu.childNodes[sectionId].style.backgroundColor="white";
+    //add background-color to active navigation
+    navMenu.childNodes[sectionId].style.backgroundColor = "#FF0000";
+  }
+};
 
+// remove the active class
+const removeActiveSection = (section) => {
+  section.classList.remove("active");
+  section.style.backgroundColor =
+    "linear-gradient(0deg, rgba(255,255,255,.1) 0%, rgba(255,255,255,.2) 100%)";
+  // extract index from section id
+  const sectionId = +section.id[section.id.length - 1] - 1;
+  //remove background-color to active navigation
+  navMenu.childNodes[sectionId].style.backgroundColor = "white";
 };
 
 //implementing the sectionActivation function
 const sectionActivation = () => {
-    sections.forEach(section => {
-        const elementOffset = offset(section);
+  sections.forEach((section) => {
+    const elementOffset = offset(section);
 
-        removeActive(section);
-        addActive(elementOffset < 150 && elementOffset >= -150, section);
-    });
+    removeActiveSection(section);
+    addActiveSection(elementOffset < 150 && elementOffset >= -150, section);
+  });
 };
-
 
 // Scroll to anchor ID using scrollTO event
 const scrolling = () => {
-    const links = document.querySelectorAll('.navbar__menu a');
-    links.forEach(link => {
-        link.addEventListener('click', () => {
-            for(i = 0 ; i < sections ; i++){
-                sections[i].addEventListener("click",sectionScroll(link));
-                link.preventDefault();
-                document.querySelector(this.getAttribute('link')).scrollIntoView({
-                    behavior: 'smooth'
-                })
-            }
+  const links = document.querySelectorAll(".navbar__menu a");
+  links.forEach((link) => {
+    link.addEventListener("click", (event) => {
+      for (i = 0; i < sections; i++) {
+        sections[i].addEventListener("click", sectionScroll(link));
+        event.preventDefault();
+        document.querySelector(this.getAttribute("link")).scrollIntoView({
+          behavior: "smooth",
         });
+      }
     });
-
+  });
 };
 
 document.addEventListener("DOMContentLoaded", () => {
-    buildNav();
-    scrolling();
+  buildNav();
+  scrolling();
 
-    window.addEventListener('scroll' ,sectionActivation);
-})
+  window.addEventListener("scroll", sectionActivation);
+});
 
 // End of Code
